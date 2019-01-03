9c527c2a64ab18ef90b20d3531aed18dc7c45f7a
Gabriel Possemozer <gossemozer2012@gmail.com>
2019-01-03 18:53:16 -0200

    First Model-Controller-View - Department

---
+++ SaleWebMvc/Models/Deparment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleWebMvc.Models
+{
+    public class Deparment
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
